0ceb3e8b8b77909f707de26eb5b591cccd375307
Sigurd Watt <sigurdwatt@Sigurds-MacBook-Pro.local>
2020-06-15 11:35:14 +0100

    lesson 12 done

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,44 @@ class Home extends StatelessWidget {
       centerTitle: true,
       backgroundColor: Colors.red[600],
     ),
-    body: Center(
+    
+    // Could use a padding widget instead of containr -- paddingWidget does not allow margins
+    body: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.end,
+      children: <Widget>[
+        Text("hello world"),
+        FlatButton(
+          onPressed: () {},
+          color: Colors.amber,
+          child: Text('Text in Button')
+        ),
+        Container(
+          color: Colors.cyan,
+          padding: EdgeInsets.all(30.0),
+          child: Text("hello")
+        ),
+      ],
+    ),
+    
+
+
+    );
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: () {},
+      //    child: Text('Click'),
+      //    backgroundColor: Colors.red[600],
+      //   ),
+  }
+} 
+
+    // Container(
+    //   padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+    //   margin: EdgeInsets.all(10),
+    //   color: Colors.grey[400],
+    //   child: Text('Test')
+    
+    // Center(
       // child: Image.asset('assets/coffee.png'),
 
       // child: Icon (
@@ -39,17 +76,8 @@ class Home extends StatelessWidget {
       //   color: Colors.lightBlue,
       // ),
     
-    child: IconButton(
-        onPressed: () {},
-        icon: Icon(Icons.alternate_email),
-        color: Colors.red[600],
-      ),
-    ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-         child: Text('Click'),
-         backgroundColor: Colors.red[600],
-        ),
-    );
-  }
-} 
+    // child: IconButton(
+    //     onPressed: () {},
+    //     icon: Icon(Icons.alternate_email),
+    //     color: Colors.red[600],
+    //   ),
