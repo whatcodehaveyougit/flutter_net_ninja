18978222e31b5b065bf4aa6a05e86c32916077ad
Sigurd Watt <sigurdwatt@Sigurds-MacBook-Pro.local>
2020-08-04 19:25:26 +0100

    Lesson 6, adding styles.  On video he adds a new font here too

--- lib/main.dart
+++ lib/main.dart
@@ -4,14 +4,24 @@ void main() => runApp(MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text("My first app"),
-          centerTitle: true
+          centerTitle: true,
+          backgroundColor: Colors.red[600],
         ),
         body: Center(
-          child: Text('Hello Ninjas')
+          child: Text(
+            'hello ninjas!',
+            style: TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 2.0,
+              color: Colors.grey[600]
+              )
+          )
         ),
         floatingActionButton: FloatingActionButton(
-          onPressed: null,
-          child: Text("Click")
+          onPressed: () {},
+          child: Text("Click"),
+          backgroundColor: Colors.red[600]
           ,),
       ),
     ));
