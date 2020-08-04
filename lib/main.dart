0407cc0298c11327befb569d913327449805a329
Sigurd Watt <sigurdwatt@Sigurds-MacBook-Pro.local>
2020-08-04 19:16:28 +0100

    Basic AppBar & Floating button made in Scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,17 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-      home: Text("hello!"),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("My first app"),
+          centerTitle: true
+        ),
+        body: Center(
+          child: Text('Hello Ninjas')
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: null,
+          child: Text("Click")
+          ,),
+      ),
     ));
